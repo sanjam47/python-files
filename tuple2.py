2c2bd37662b7e507a30479adec7fabdf49b919ce
sanjam47 <sanjam.khanuja@47billion.com>
2024-01-24 14:27:42 +0530

    added

---
+++ tuple2.py
@@ -0,0 +1,14 @@
+tuple1=(1,2,3)
+
+print(tuple)
+print(type(tuple))
+
+
+tuple2=(1)
+print(type(tuple2))
+
+#for one item we have to add a comma(,).
+tuple3=tuple((1,))
+print(type(tuple3))
+print(len(tuple3))
+
