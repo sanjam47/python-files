c024393f6bf80a9e774b84b235e6d2cd534678b7
sanjam47 <sanjam.khanuja@47billion.com>
2024-01-24 15:26:44 +0530

    updated

--- tuple2.py
+++ tuple2.py
@@ -12,3 +12,32 @@ tuple3=tuple((1,))
 print(type(tuple3))
 print(len(tuple3))
 
+tuple4=tuple(("apple","watermelon","banana"))
+print(tuple4)
+print(len(tuple4))
+list1=list(tuple4)
+y=list1[1]
+print(y)
+list1.append('cherry')
+tuple4=list(list1)
+print(tuple4)
+list1.pop()
+tuple4=tuple(list1)
+print(tuple4)
+
+(a,b,c)=tuple4
+print(a,b,c)
+
+for i in range(len(tuple4)):
+    print(tuple4[i],' ')
+
+#tuple functions 
+print(tuple4)
+print(tuple4.count('banana'))
+print(tuple4.index('banana'))
+
+tuple5=('banana',)
+tuple4+=tuple5
+print(tuple4)
+print(tuple4.count('banana'))
+print(tuple4.index('banana',))
